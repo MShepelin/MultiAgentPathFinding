0e52a2254f4c00376d6398a9c9d0d759a3583531
Dmitry Shepelin <shepelin.d@ya.ru>
2020-11-10 17:16:35 +0300

    Removed build files

--- Build/Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/shepe/CProjects/MultiAgentPathFinding
-# Build directory: C:/Users/shepe/CProjects/MultiAgentPathFinding/Build/Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Src")
-subdirs("Tests")
--- Build/Debug/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/shepe/CProjects/MultiAgentPathFinding/Src
-# Build directory: C:/Users/shepe/CProjects/MultiAgentPathFinding/Build/Debug/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
